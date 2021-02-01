a833b9488a636c0e33e1df71c95433bf8bc25694
s4nt14go <santiagopergamo@gmail.com>
2021-02-01 00:14:25 -0300

    ci: debugging ConfigError: Missing region in config

--- tests/steps/init.js
+++ tests/steps/init.js
@@ -1,6 +1,6 @@
+require('dotenv').config()
 const { promisify } = require('util')
 const awscred = require('awscred')
-require('dotenv').config()
 
 let initialized = false
 
@@ -10,10 +10,13 @@ const init = async () => {
   }
 
   const { credentials, region } = await promisify(awscred.load)()
+  console.info('credentials', credentials);
+  console.info('region', region);
 
   process.env.AWS_ACCESS_KEY_ID     = credentials.accessKeyId
   process.env.AWS_SECRET_ACCESS_KEY = credentials.secretAccessKey
   process.env.AWS_REGION            = region
+  console.info('process.env.AWS_REGION', process.env.AWS_REGION);
 
   if (credentials.sessionToken) {
     process.env.AWS_SESSION_TOKEN = credentials.sessionToken
