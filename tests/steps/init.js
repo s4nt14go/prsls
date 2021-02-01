ab48ff4c93eb652f9248c746333b8fb54c50d412
s4nt14go <santiagopergamo@gmail.com>
2021-02-01 00:37:07 -0300

    revert: commit a833b948 "ci: debugging ConfigError: Missing region in config"
    Remove console.logging

--- tests/steps/init.js
+++ tests/steps/init.js
@@ -10,13 +10,10 @@ const init = async () => {
   }
 
   const { credentials, region } = await promisify(awscred.load)()
-  console.info('credentials', credentials);
-  console.info('region', region);
 
   process.env.AWS_ACCESS_KEY_ID     = credentials.accessKeyId
   process.env.AWS_SECRET_ACCESS_KEY = credentials.secretAccessKey
   process.env.AWS_REGION            = region
-  console.info('process.env.AWS_REGION', process.env.AWS_REGION);
 
   if (credentials.sessionToken) {
     process.env.AWS_SESSION_TOKEN = credentials.sessionToken
